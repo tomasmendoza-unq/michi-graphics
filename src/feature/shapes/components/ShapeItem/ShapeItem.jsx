19fb1293273a80a47168cfa4aaf30dd2798f9b62
Tomas Mendoza <tm1453766@gmail.com>
2026-07-30 17:45:45 -0300

    fix: se acomodan las piezas

--- src/feature/shapes/components/ShapeItem/ShapeItem.jsx
+++ src/feature/shapes/components/ShapeItem/ShapeItem.jsx
@@ -1,22 +1,40 @@
 import { Stage, Layer } from "react-konva";
 import { ShapeRenderer } from "../ShapeRender/ShapeRender";
 
+const PREVIEW_SIZE = 60;
+
+const getPreviewProps = (shape) => {
+    const centered = { x: PREVIEW_SIZE / 2, y: PREVIEW_SIZE / 2 };
+
+    switch (shape.type) {
+        case "rect":
+            return { ...shape, x: 10, y: 15, width: 40, height: 30 };
+        case "circle":
+            return { ...shape, x: centered.x, y: centered.y, radius: 20 };
+        case "line":
+            return { ...shape, points: [10, 50, 50, 10] };
+        default:
+            return shape;
+    }
+};
+
 export const ShapeItem = ({ shape, onClick }) => {
+    const previewShape = getPreviewProps(shape);
+
     return (
         <button
             className="shape-item"
             onClick={() => onClick?.(shape)}
         >
             <Stage
-                width={60}
-                height={60}
+                width={PREVIEW_SIZE}
+                height={PREVIEW_SIZE}
                 className="shape-item__preview"
             >
                 <Layer>
-                    <ShapeRenderer shape={shape} />
+                    <ShapeRenderer shape={previewShape} />
                 </Layer>
             </Stage>
-
             <span>{shape.name}</span>
         </button>
     );
